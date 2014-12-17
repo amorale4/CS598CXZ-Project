05c1bc66afde6ade47b6b19fde3d0e7b09354521
Alex <amorale4@illinois.edu>
2014-12-17 15:59:11 -0600

    file corpus updated

--- pysrc/file_corpus.py
+++ pysrc/file_corpus.py
@@ -26,15 +26,61 @@ def main(args):
 	productTim = "review/time:"
 	productSum = "review/summary:" 
 	productRev = "review/text:"
-	pr_len = len(prodRef)
+	pr_len = len(productRef)
+	product_list = []
 	with open(args[0], 'r') as f:
-		current_product = ""
-		product_meta = []
-		current_product_list = []
+		current_product = ""      #current product name
+		product_meta = []         #current review for the product
+		current_product_list = [] #list of reviews for this product
+		i = 0
 		for line in f:
-			if len(line) > 0 and line[:pr_len]== productRef:	
-			
-				
+			#if i > 1000: break
+			#i = i+1
+			if len(line) > 0 and line[:pr_len]== productRef:
+				product = line[pr_len:].strip()
+				if product == current_product:
+					#add the current product
+					if( len(product_meta) > 0):
+						current_product_list.append(",".join(product_meta) + "\n")
+						product_meta = []
+					
+				else:
+					if len(current_product_list) > 0:
+						if( len(product_meta) > 0):
+							current_product_list.append(",".join(product_meta) + "\n")
+							product_meta = []
+						
+						with open(args[1] + current_product + ".txt", 'w') as output:
+							output.writelines(current_product_list)
 
+					current_product = product
+					if not current_product in product_list:
+						#print current_product
+						product_list.append(current_product)
+
+					else:
+						print "have already seen: "+ current_product	
+					current_product_list = []
+					#write the lines to file
+					#print "add to file"
+					
+			elif len(line) > 0:
+				if line[:len(productNam)] == productNam:
+					product_meta.append( line[len(productNam):].strip() )				
+				elif line[:len(productHlp)] == productHlp:		
+					product_meta.append( line[len(productHlp):].strip() )
+				elif line[:len(productScr)] == productScr:		
+					product_meta.append( line[len(productScr):].strip() )
+				elif line[:len(productTim)] == productTim:	
+					product_meta.append( line[len(productTim):].strip() )	
+				elif line[:len(productSum)] == productSum:	
+					product_meta.append( line[len(productSum):].strip().replace(",", "" ) )
+				elif line[:len(productRev)] == productRev:		
+					product_meta.append( "\"" +  line[len(productRev):].strip() + "\"" )
+
+	if len(current_product_list) > 0:
+		with open(args[1] + current_product + ".txt", 'w') as output:
+			output.writelines(current_product_list)
+		
 if __name__ == "__main__":
 	main(sys.argv[1:])
